8ab1ef3afbee946207b1845f0bda66da02af62fe
rwadnd <0rwad.nadam0@gmail.com>
2022-10-11 17:43:43 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+
+for(let i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+      handleClick(this.innerHTML)
+    })
+}
+
+
+
+document.addEventListener("keydown", function(event){
+  handleClick(event.key)
+})
+
+
+
+function handleClick(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3")
+      tom1.play();
+      break;
+  
+     case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3")
+      tom2.play();
+      break;
+     
+     case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3")
+      tom3.play();
+      break;
+
+     case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3")
+      tom4.play();
+      break;
+
+     case "l":
+      var crash = new Audio("sounds/aagh.mp3")
+      crash.play();
+      break;
+
+      case "k":
+      var kick_bass = new Audio("sounds/kick-bass.mp3")
+      kick_bass.play();
+      break;
+
+      case "j":
+        var snare = new Audio("sounds/snare.mp3")
+        snare.play();
+        break;
+    default:
+      console.log(clicked)
+      break;
+  }
+}
+
+
