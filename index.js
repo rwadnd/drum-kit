c4152b38d5c78f9257e1c51c1e14f9375228a8f0
rwadnd <0rwad.nadam0@gmail.com>
2022-10-11 17:58:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,42 +11,47 @@ document.addEventListener("keydown", function(event){
   handleClick(event.key)
 })
 
+var tom1 = new Audio("sounds/tom-1.mp3")
+var tom2 = new Audio("sounds/tom-2.mp3")
+var tom3 = new Audio("sounds/tom-3.mp3")
+var tom4 = new Audio("sounds/tom-4.mp3")
+var crash = new Audio("sounds/aagh.mp3")
+var kick_bass = new Audio("sounds/crash.mp3")
+var snare = new Audio("sounds/snare.mp3")
 
 
 function handleClick(key){
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3")
       tom1.play();
       break;
   
      case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3")
+  
       tom2.play();
       break;
      
      case "s":
-      var tom3 = new Audio("sounds/tom-3.mp3")
       tom3.play();
       break;
 
      case "d":
-      var tom4 = new Audio("sounds/tom-4.mp3")
+      
       tom4.play();
       break;
 
      case "l":
-      var crash = new Audio("sounds/aagh.mp3")
+      
       crash.play();
       break;
 
       case "k":
-      var kick_bass = new Audio("sounds/crash.mp3")
+      
       kick_bass.play();
       break;
 
       case "j":
-        var snare = new Audio("sounds/snare.mp3")
+        
         snare.play();
         break;
     default:
